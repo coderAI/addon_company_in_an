601861d3db466d843147fc5410c1bfa957c045a0
quanne <quanvm00@gmail.com>
2019-12-13 11:09:37 +0700

    [UPDATE] so 7

--- working_order/models/account_invoice.py
+++ working_order/models/account_invoice.py
@@ -5,3 +5,14 @@ from odoo import fields, models, api
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
+
+    @api.multi
+    def action_invoice_paid(self):
+        res = super(AccountInvoice, self).action_invoice_paid()
+        sale_obj = self.env['sale.order']
+        for inv in self:
+            if inv.origin:
+                for so in sale_obj.search[('name', '=', inv.origin)]:
+                    if so.state in ['draft', 'sent', 'sale']:
+                        so.write({'state':'paid'})
+        return res
