adf9c7b7c16d5349a10a2e782426028ddc55d81c
quanvm00 <quanvm00@gmail.com>
2019-12-28 18:45:34 +0700

    [UPDATE] api paid so

--- working_order/controllers/main.py
+++ working_order/controllers/main.py
@@ -8,6 +8,37 @@ _logger = logging.getLogger(__name__)
 
 
 class APISaleOrder(http.Controller):
+    @http.route('/web/api/paid_so', type='json', auth='none', methods=['post'], csrf=False)
+    def api_paid_so(self, **kw):
+        # data = {
+        #     'barcode' : '112233'
+        # }
+        wol_obj = request.env['work.order.line'].sudo()
+        so_obj = request.env['sale.order'].sudo()
+        sol_obj = request.env['sale.order.line'].sudo()
+        ###############
+        params = request.params.copy()
+        data = request.jsonrequest
+        barcode = data.get('barcode', '')
+
+        for wol in wol_obj.search([('bar_code', '=', barcode)], limit=1):
+            wol.write({'state': 'done'})
+            wo = wol.work_order_id.sudo()
+            check_wol_another_in_same_wo = wo.work_order_line_ids.filtered(lambda r: r.state == 'draft')
+            if not check_wol_another_in_same_wo:
+                wo.write({'state': 'done'})
+                sol = wol.sale_order_line_id.sudo()
+                check_wol_another_in_same_sol = wol_obj.search([('sale_order_line_id', '=', sol.od),
+                                                                ('state', '=', 'draft'), ])
+                if not check_wol_another_in_same_sol:
+                    sol.write({'state_new': 'done'})
+                    so = sol.order_id
+
+                    check_sol_in_same_so = so.order_line.filtered(lambda r: r.state == 'draft')
+                    if not check_sol_in_same_so:
+                        sol.write({'state': 'to delivery'})
+
+        return True
 
     @http.route('/web/api/create_so', type='json', auth='none', methods=['post'], csrf=False)
     def api_create_so(self, **kw):
@@ -47,7 +78,10 @@ class APISaleOrder(http.Controller):
         if not journal:
             data_error.append({'journal': 'No journal with reference %s' % journal_code})
 
-
+        if tax_code:
+            tax = tax_code.search([('code', '=', tax_code)], limit=1)
+            if not tax:
+                data_error.append({'tax_code': 'No tax code with  %s' % journal_code})
 
         for line in order_lines:
             product = product_obj.search([('default_code', '=', line.get('product_code', ''))], limit=1)
@@ -55,14 +89,15 @@ class APISaleOrder(http.Controller):
                 data_error.append({'product': 'No Product with default_code %s' % line.get('product_code', '')})
             else:
                 line.update({'product': product})
-                data_product.append((0, 0, {
+                vals_product = {
                     'name': product.name,
                     'product_id': product.id,
                     'product_uom_qty': line.get('qty', 0.0),
                     'product_uom': product.uom_id.id,
                     'price_unit': line.get('price_unit', 0.0),
                     'tax_id': [(6, 0, [])],
-                }), )
+                }
+                data_product.append((0, 0, vals_product), )
 
         if data_error:
             return data_error
